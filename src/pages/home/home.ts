fb35ac4c37da9b014a3bf02b034e939306383f41
Arek Ruta <arek.ruta89@gmail.com>
2017-07-31 19:35:45 +0200

    getPermissions startListening

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +7,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  matches: String[];
+  isRecording: boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private speechRecognition: SpeechRecognition
+  ) {}
 
+  getPermissions() {
+    this.speechRecognition.hasPermission().then((hasPermission: boolean) => {
+      if (!hasPermission) {
+        this.speechRecognition.requestPermission();
+      }
+    });
   }
 
+  startListening() {
+    let options = {
+      language: 'pl-PL'
+    };
+
+    this.speechRecognition.startListening(options).subscribe(matches => {
+      this.matches = matches;
+    });
+    this.isRecording = true;
+  }
+  
 }
